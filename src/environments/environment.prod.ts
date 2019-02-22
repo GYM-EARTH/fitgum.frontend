07f9054d91af14e09f13a6498e17b2d80cc8f149
e0eee <dmitriy_girskiy@yahoo.com>
2019-02-22 11:45:08 +0300

    customize production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  passport: 'https://api.fitgum.ru/login',
+  clubs: 'https://api.fitgum.ru/clubs',
+  news: 'https://api.fitgum.ru/news',
+  trainers: 'https://api.fitgum.ru/trainers'
 };
