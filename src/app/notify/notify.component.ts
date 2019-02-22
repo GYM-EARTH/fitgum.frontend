98c4371aaa0dcf7c318c81b772763c358a3d8a71
e0eee <dmitriy_girskiy@yahoo.com>
2019-02-22 13:58:30 +0300

    disable notify

--- src/app/notify/notify.component.ts
+++ src/app/notify/notify.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from '../cookie.service';
 
 @Component({
   selector: 'app-notify',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotifyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cookieService: CookieService) { }
 
   ngOnInit() {
+    this.cookieService.setCookie('notify', 'true', {'expires': 7});
   }
 
 }
